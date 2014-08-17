dabe182c512b617a2d9734e6651ef3685cb79a67
Wonkybadank <zthurman@users.noreply.github.com>
2014-08-17 15:42:26 -0700

    Update GenODE.py

--- GenODE.py
+++ GenODE.py
@@ -33,7 +33,7 @@ def rk4(odes, state, parameters, dt=0.01):
 
 # Alternate RK4 function
 
-def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):  
+"""def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):  
     tsp = np.arange(t0, t1, dt)
     Nsize = np.size(tsp)
     X = np.empty((Nsize, np.size(x0)))
@@ -45,43 +45,43 @@ def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
         k3 = ng(X[i-1] + dt/2*k2, tsp[i-1] + dt/2)
         k4 = ng(X[i-1] + dt*k3, tsp[i-1] + dt)
         X[i] = X[i-1] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
-    return X
+    return X"""
 
 # Function that holds the initial conditions and parameters of the ODE
 
-def ODEs((x), ()):
+def ODEs((x), (omega)):
     return np.array([sin(x)])
 
 # Function that calls the generate function that generates data from the system
 
 def ODE_generate(data_length):
     return generate(data_length, ODEs, \
-            	    np.array([3.0, 0, -1.2]), np.array([1.0, 3.0, 1.0, 5.0, 0.006, 4.0, 1.3, -1.5]))
+            	    np.array([0]), np.array([60]))
 
 # Function that creates the phase plot for the system
 
-def do_pplot():
+"""def do_pplot():
     pylab.figure()
     data = ODE_generate(10000)   # how long the function is solved for
     pylab.plot(data[0,:], data[1,:])
-    pylab.title("Phase Portrait - Hindmarsh-Rose")
-    pylab.xlabel("Membrane Potential")
-    pylab.ylabel("Membrane Recovery Variable")
+    pylab.title("Phase Portrait - ODE")
+    pylab.xlabel("X Dynamical Variable")
+    pylab.ylabel("Y Dynamical Variable")
     pylab.show()
 
-print do_pplot()
+print do_pplot()"""
 
 # Function that creates the time plot for the system
 
 def do_tplot():
     pylab.figure()
     data = ODE_generate(10000)   # how long the function is solved for
-    pylab.plot(data[0,:])
-    pylab.title("Membrane Potential over Time - single uncoupled neuron")
+    pylab.plot(data[0])
+    pylab.title("ODE Solution")
     pylab.xlabel("Time")
-    pylab.ylabel("Membrane Potential")
+    pylab.ylabel("Numerically approximated solution")
     pylab.xlim(0,10000)
-    pylab.ylim(-1.5,3.5)
+    pylab.ylim(-1,1)
     pylab.show()
-
-print do_tplot()
+    
+print do_tplot()    
