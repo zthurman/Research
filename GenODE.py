027906492c22d24e9ee2d1226b54fc2a7ed0ac99
Wonkybadank <zthurman@users.noreply.github.com>
2014-08-17 11:35:48 -0700

    Update GenODE.py

--- GenODE.py
+++ GenODE.py
@@ -11,6 +11,8 @@ import sys
 import math as mt
 from scipy.integrate import ode
 
+# Function used for generating the  data, calls RK4 function and used in ODE_generate function
+
 def generate(data_length, odes, state, parameters):
     data = np.zeros([state.shape[0], data_length])
     for i in xrange(1):
@@ -20,12 +22,16 @@ def generate(data_length, odes, state, parameters):
         data[:, i] = state
     return data
 
+# Function called in the generate function, takes ODEs function as an argument 
+
 def rk4(odes, state, parameters, dt=0.01):
     k1 = dt * odes(state, parameters)
     k2 = dt * odes(state + 0.5 * k1, parameters)
     k3 = dt * odes(state + 0.5 * k2, parameters)
     k4 = dt * odes(state + k3, parameters)
     return state + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+# Alternate RK4 function
 
 def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):  
     tsp = np.arange(t0, t1, dt)
@@ -41,7 +47,41 @@ def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
         X[i] = X[i-1] + dt/6*(k1 + 2*k2 + 2*k3 + k4)
     return X
 
-def HR_odes((x,y,z), (a, b, c, d, r, s, I, xnot)):
-    return np.array([y - a*(x**3) + (b*(x**2)) - z + I, \
-                        c - d*(x**2) - y, \
-                        r*(s*(x - xnot) - z)])
+# Function that holds the initial conditions and parameters of the ODE
+
+def ODEs((x), ()):
+    return np.array([sin(x)])
+
+# Function that calls the generate function that generates data from the system
+
+def ODE_generate(data_length):
+    return generate(data_length, ODEs, \
+            	    np.array([3.0, 0, -1.2]), np.array([1.0, 3.0, 1.0, 5.0, 0.006, 4.0, 1.3, -1.5]))
+
+# Function that creates the phase plot for the system
+
+def do_pplot():
+    pylab.figure()
+    data = ODE_generate(10000)   # how long the function is solved for
+    pylab.plot(data[0,:], data[1,:])
+    pylab.title("Phase Portrait - Hindmarsh-Rose")
+    pylab.xlabel("Membrane Potential")
+    pylab.ylabel("Membrane Recovery Variable")
+    pylab.show()
+
+print do_pplot()
+
+# Function that creates the time plot for the system
+
+def do_tplot():
+    pylab.figure()
+    data = ODE_generate(10000)   # how long the function is solved for
+    pylab.plot(data[0,:])
+    pylab.title("Membrane Potential over Time - single uncoupled neuron")
+    pylab.xlabel("Time")
+    pylab.ylabel("Membrane Potential")
+    pylab.xlim(0,10000)
+    pylab.ylim(-1.5,3.5)
+    pylab.show()
+
+print do_tplot()
