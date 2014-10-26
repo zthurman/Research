73a120090c1f5f51fa152de1112203904a2a0d75
wonkybadonk <zthurman@users.noreply.github.com>
2014-10-26 16:00:57 -0700

    Update ballisticfizz.py

--- ballisticfizz.py
+++ ballisticfizz.py
@@ -23,13 +23,13 @@ def RK4(t0 = 0, x0 = np.array([1]), t1 = 5 , dt = 0.01, ng = None):
 
 def ballistix(x,t, gamma = 0.25, m = 0.028, D = 0.0056, g = 9.8):
     c = gamma*(D**2)
-    return np.array([ -c*sqrt(x[0]**2 + x[1]**2)*x[0], \
-                    -m*g - c*sqrt(x[0]**2 + x[1]**2)*x[1]])
+    return np.array([ -(c*sqrt(x[0]**2 + x[1]**2)*x[0])/m, \
+                    -g - (c*sqrt(x[0]**2 + x[1]**2)*x[1])/m])
 
 def do_tplot():
-    X = RK4(x0 = np.array([0,444]), t1 = 2500,dt = 0.1, ng = ballistix)
+    X = RK4(x0 = np.array([0,444]), t1 = 200, dt = 0.1, ng = ballistix)
     t0 = 0
-    t1 = 2500
+    t1 = 200
     dt = 0.1
     tsp = np.arange(t0, t1, dt)
     pylab.plot(tsp,X[:,1])
@@ -44,13 +44,13 @@ print do_tplot()
 
 def ballistix(x,t, gamma = 0.25, m = 0.159, D = 0.0091, g = 9.8):
     c = gamma*(D**2)
-    return np.array([ -c*sqrt(x[0]**2 + x[1]**2)*x[0], \
-                    -m*g - c*sqrt(x[0]**2 + x[1]**2)*x[1]])
+    return np.array([ -(c*sqrt(x[0]**2 + x[1]**2)*x[0])/m, \
+                    -g - (c*sqrt(x[0]**2 + x[1]**2)*x[1])/m])
 
 def do_tplot():
-    X = RK4(x0 = np.array([0,429]), t1 = 1000,dt = 0.1, ng = ballistix)
+    X = RK4(x0 = np.array([0,429]), t1 = 200,dt = 0.1, ng = ballistix)
     t0 = 0
-    t1 = 1000
+    t1 = 200
     dt = 0.1
     tsp = np.arange(t0, t1, dt)
     pylab.plot(tsp,X[:,1])
@@ -65,13 +65,13 @@ print do_tplot()
 
 def ballistix(x,t, gamma = 0.25, m = 0.252, D = 0.0114, g = 9.8):
     c = gamma*(D**2)
-    return np.array([ -c*sqrt(x[0]**2 + x[1]**2)*x[0], \
-                    -m*g - c*sqrt(x[0]**2 + x[1]**2)*x[1]])
+    return np.array([ -(c*sqrt(x[0]**2 + x[1]**2)*x[0])/m, \
+                    -g - (c*sqrt(x[0]**2 + x[1]**2)*x[1])/m])
 
 def do_tplot():
-    X = RK4(x0 = np.array([0,309]), t1 = 1000,dt = 0.1, ng = ballistix)
+    X = RK4(x0 = np.array([0,309]), t1 = 200,dt = 0.1, ng = ballistix)
     t0 = 0
-    t1 = 1000
+    t1 = 200
     dt = 0.1
     tsp = np.arange(t0, t1, dt)
     pylab.plot(tsp,X[:,1])
@@ -86,19 +86,20 @@ print do_tplot()
 
 def ballistix(x,t, gamma = 0.25, m = 0.710, D = 0.0127, g = 9.8):
     c = gamma*(D**2)
-    return np.array([ -c*sqrt(x[0]**2 + x[1]**2)*x[0], \
-                    -m*g - c*sqrt(x[0]**2 + x[1]**2)*x[1]])
+    return np.array([ -(c*sqrt(x[0]**2 + x[1]**2)*x[0])/m, \
+                    -g - (c*sqrt(x[0]**2 + x[1]**2)*x[1])/m])
 
 def do_tplot():
-    X = RK4(x0 = np.array([0,900.2]), t1 = 1000,dt = 0.1, ng = ballistix)
+    X = RK4(x0 = np.array([0,900.2]), t1 = 400,dt = 0.1, ng = ballistix)
     t0 = 0
-    t1 = 1000
+    t1 = 400
     dt = 0.1
     tsp = np.arange(t0, t1, dt)
     pylab.plot(tsp,X[:,1])
     pylab.title("Y velocity over time, .50 BMG")
     pylab.xlabel("Time")
     pylab.ylabel("Vertical velocity")
+    pylab.ylim(-500,1000)
     pylab.savefig("50 BMG Figure.png")
     pylab.show()
     return
@@ -107,13 +108,13 @@ print do_tplot()
 
 def ballistix(x,t, gamma = 0.25, m = 0.3, D = 0.0185, g = 9.8):
     c = gamma*(D**2)
-    return np.array([ -c*sqrt(x[0]**2 + x[1]**2)*x[0], \
-                    -m*g - c*sqrt(x[0]**2 + x[1]**2)*x[1]])
+    return np.array([ -(c*sqrt(x[0]**2 + x[1]**2)*x[0])/m, \
+                    -g - (c*sqrt(x[0]**2 + x[1]**2)*x[1])/m])
 
 def do_tplot():
-    X = RK4(x0 = np.array([0,396.2]), t1 = 1000,dt = 0.1, ng = ballistix)
+    X = RK4(x0 = np.array([0,396.2]), t1 = 200, dt = 0.1, ng = ballistix)
     t0 = 0
-    t1 = 1000
+    t1 = 200
     dt = 0.1
     tsp = np.arange(t0, t1, dt)
     pylab.plot(tsp,X[:,1])
@@ -128,13 +129,13 @@ print do_tplot()
 
 def ballistix(x,t, gamma = 0.25, m = 4.672, D = 0.1173, g = 9.8):
     c = gamma*(D**2)
-    return np.array([ -c*sqrt(x[0]**2 + x[1]**2)*x[0], \
-                    -m*g - c*sqrt(x[0]**2 + x[1]**2)*x[1]])
+    return np.array([ -(c*sqrt(x[0]**2 + x[1]**2)*x[0])/m, \
+                    -g - (c*sqrt(x[0]**2 + x[1]**2)*x[1])/m])
 
 def do_tplot():
-    X = RK4(x0 = np.array([0,452.93]), t1 = 110,dt = 0.1, ng = ballistix)
+    X = RK4(x0 = np.array([0,452.93]), t1 = 120,dt = 0.1, ng = ballistix)
     t0 = 0
-    t1 = 110
+    t1 = 120
     dt = 0.1
     tsp = np.arange(t0, t1, dt)
     pylab.plot(tsp,X[:,1])
